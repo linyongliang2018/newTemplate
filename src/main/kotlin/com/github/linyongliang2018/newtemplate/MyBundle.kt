db117ddae518f01547ad91abe7f57ceb6b4ffc01
GitHub Action <action@github.com>
2023-09-14 16:35:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/linyongliang2018/newtemplate/MyBundle.kt
+++ src/main/kotlin/com/github/linyongliang2018/newtemplate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.linyongliang2018.newtemplate
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/linyongliang2018/newtemplate/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/linyongliang2018/newtemplate/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.linyongliang2018.newtemplate.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
